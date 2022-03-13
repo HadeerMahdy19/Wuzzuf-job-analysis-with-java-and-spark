d0596f6cf91a2d4689eb5846627863105046da40
maryam <maryam1998.mostafa@gmail.com>
2022-03-13 17:27:23 +0200

    commit 3 : change in spark version in pom file and added some functions of task3 in job service class

--- src/main/java/com/WuzzufJobAnalysis/job/jobController.java
+++ src/main/java/com/WuzzufJobAnalysis/job/jobController.java
@@ -10,20 +10,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.LinkedHashMap;
+
 @RequestMapping("spark-context")
 @Controller
 public class jobController {
 
     @RequestMapping("read-csv")
     public ResponseEntity<String> getRowCount() {
+        jobService service = new jobService();
+        LinkedHashMap<String, Integer> lhm =  service.getFeatureValuesCount("Title");
+        //System.out.println("*********************\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n ");
+        //System.out.println(lhm);
+        //System.out.println("*********************\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n ");
         Dataset<jobPOJO> dataset = new jobDAO().prepareData();
-        System.out.println("***********************************************************");
-        dataset.show();
-        String html = String.format("<h1>%s</h1>", "Running Apache Spark on/with support of Spring boot") +
+       // System.out.println("***********************************************************");
+        //dataset.show();
+        /*String html = String.format("<h1>%s</h1>", "Running Apache Spark on/with support of Spring boot") +
                 String.format("<h3>%s</h3>", "Read csv..") +
 //                String.format("<h4>Total records %d</h4>", dataset.count()) +
                 String.format("<h5>Schema <br/> %s</h5> <br/> Sample data - <br/>", dataset.schema().treeString()) +
                 dataset.showString(20, 20, true);
+        return ResponseEntity.ok(html);*/
+        String html = String.format("<h1>%s</h1>", "Running Apache Spark on/with support of Spring boot") +
+                String.format("<h3>%s</h3>", "Read csv..") +
+//                String.format("<h4>Total records %d</h4>", dataset.count()) +
+                String.format("<h5>Schema <br/> %s</h5> <br/> hashmap <br/>", lhm.values().toString());
         return ResponseEntity.ok(html);
     }
 }
